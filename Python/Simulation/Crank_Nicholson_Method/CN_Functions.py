a45ec0507dda4b709bcb3925efe03977a2d96f7e
Alfred Coulébétouba <alfredcoulebe@live.fr>
2022-08-27 19:49:20 -0400

    On a fini la simulation et l'affichage 3D mais le drawTab semble avoir du mal a être lu donc les tirages n'apparaissent pas. A corriger

--- Python/Simulation/Crank_Nicholson_Method/CN_Functions.py
+++ Python/Simulation/Crank_Nicholson_Method/CN_Functions.py
@@ -13,7 +13,7 @@ class Tank:
         Dc: Thermal diffusity coefficient (m²/s)
         UL: Thermal losses coefficient (s⁻¹)'''
     def  __init__(self, Vol_, H_, UL_, Dc_ =0.14558e-6, Rho_ = 1e3, Cv_ =  4185.5):
-        self.vol = Vol_
+        self.Vol = Vol_
         self.H = H_
         self.Cv = Cv_
         self.Rho = Rho_
@@ -56,15 +56,15 @@ def HELayer(N_layer, deltaX, pos_vec):
     
     N = np.size(pos_vec)
     count = 0
-    tab_pos = -1*np.ones(N)
+    tab_pos = np.empty(N, dtype= int)
     for i in range(N):
         for j in range(N_layer):
             if j*deltaX <= pos_vec[i] < (j+1)*deltaX:
-              tab_pos[i] = j
+              tab_pos[i] = int(j)
               count += 1
               break
           
-    if(count != N):
+    if(count == N):
         return np.unique(tab_pos)
     else:
         print("Some HE positions are out of boundaries")
@@ -89,7 +89,7 @@ def matrix(N_layer, V, deltaX, deltaT, eps, heatState, Tank, HE):
         '''
     ##Coefficients
     m_i = Tank.Vol*Tank.Rho/N_layer
-    e = eps + Tank.Dc/(2 * (deltaX**2))
+    e = eps * Tank.Dc/(2 * (deltaX**2))
     d = V/(4 * deltaX)
     q1 = e+ d
     q2 = 1/deltaT + 2*e +Tank.UL/(2*N_layer)
@@ -103,26 +103,26 @@ def matrix(N_layer, V, deltaX, deltaT, eps, heatState, Tank, HE):
     
     for i in range(1, N_layer - 1):
         Z1[i, i-1:i+2] = [-q1, q2, q3]
-        Z1[i, N_layer+1] = -Tank.UL/N_layer
-    Z1[N_layer, N_layer - 2:N_layer+1] = [1, -4, 3]
+        Z1[i, N_layer] = -Tank.UL/N_layer
+    Z1[N_layer-1, N_layer - 3:N_layer] = [1, -4, 3]
      
-    Z1[N_layer+1,N_layer+1] = 1
+    Z1[N_layer,N_layer] = 1
     
     ##Generate Z2
     Z2 = np.zeros((N_layer+1, N_layer+1))
-    Z1[0,0] = 1
+    Z2[0,0] = 1
     
     for i in range(1, N_layer - 1):
-        Z1[i, i-1:i+2] = [q1, q4, -q3]
+        Z2[i, i-1:i+2] = [q1, q4, -q3]
    
-    Z1[N_layer+1,N_layer+1] = 1
+    Z2[N_layer,N_layer] = 1
     
     ##Generate Z3
-    Z3 = np.zeros((N_layer+1, 1))
+    Z3 = np.zeros((N_layer+1))
     Positions_index = HELayer(N_layer, deltaX, HE.Positions) #Consider that HE position were already sorted
     for i in range(HE.N):
         pos = Positions_index[i]
-        Z3[pos, :] = HE.n_eff * HE.Power/(m_i * Tank.Cv) * heatState[i]  #K/s
+        Z3[pos] = HE.n_eff * HE.Power/(m_i * Tank.Cv) * heatState[i]  #K/s
     return Z1, Z2, Z3
 
 #%%
@@ -142,10 +142,35 @@ def PowerState(T_vec, HE, Target, deltaX, N_layers):
     heatState = np.zeros(HE.N) #1= ON/ 0 = OFF
     Thermos_index = HELayer(N_layers, deltaX, HE.Thermos) #Consider that thermos positions were already sorted 
     for i in reversed(range(HE.N)):
-        if T_vec[Thermos_index] < Target:
+        if T_vec[Thermos_index[i]] < Target:
             heatState[i] = 1
             break
     return heatState
+
+#%%
+def DrawRate(t, Tank, DrawTab):
+    '''
+    Mean to determine the draw rate at each moment given the draw tab
+    Parameters:
+        t: time (s)
+        Tank: Object having the tank Caracteristics(Cf higher for more details)
+        DrawTab: Tab having the schedule of all draw with ['Start_time(h), duration(min), Debit(L/min)']
+    Outputs: Value of Draw Rate(m/s)
+        '''
+    I = np.size(DrawTab, 0)
+    if I:
+        for i in reversed(range(I)):
+            if t >= DrawTab[i,1] *3600:
+                break
+            if(t >= DrawTab[i,1] *3600) and (t <= DrawTab[i,1] *3600 + DrawTab[i,2] *60):
+                V = DrawTab[i,3] * 1e-3 * Tank.H/(Tank.Vol*60)
+            else:
+                V = 0
+    else:
+         V = 0
+                 
+    return V
+    
 
 #%%
 
@@ -169,12 +194,39 @@ def CN_meth(Tank, HeatElem, Draw_Tab, deltaT, sim_time,N_layer, T_init, T_amb, T
     Outputs:
         tsol: solution time vector(1xM)(sec)
         xVector: Space vector (1xN_layers)(m)
-        sol: Solution matrice(MxN)
-        
-        
+        sol: Solution matrice(NxM)
         '''
+    #Time array
+    tsol = np.arange(0, sim_time*3600, deltaT)
+    M = np.size(tsol)
+    #Space Vector
+    xVector = np.linspace(0, Tank.H, N_layer)
+    #Simulation parameters
+    deltaX = Tank.H/N_layer
+    sol = np.zeros((N_layer+1, M))
     
+    #Initialisation
+    sol[:,0] = np.concatenate((T_init * np.ones((N_layer)), np.array([T_amb])), axis = 0)
+    #simulation
+    for i in range(1,M):
+        
+        V  = DrawRate(tsol[i], Tank, Draw_Tab)
+        
+        heatState = PowerState(sol[:, i-1], HeatElem, T_target, deltaX, N_layer)
+        
+        Z1, Z2, Z3 = matrix(N_layer, V, deltaX, deltaT, eps, heatState, Tank, HeatElem)
+        #Update
+        sol[:,i] = np.linalg.inv(Z1)@(Z2@ sol[:, i-1] + Z3)
+        #Set T1 value given V
+        if V > 0 :
+            sol[0,i] = T_in
+        else:
+            sol[0,i] = (4*sol[1,i] - sol[2,i])/ 3
+            
+            
+        
     
+    return tsol, xVector, sol
     
     
     
--- Python/Simulation/Crank_Nicholson_Method/Crank_Nicolson_Simulation.py
+++ Python/Simulation/Crank_Nicholson_Method/Crank_Nicolson_Simulation.py
@@ -1,8 +1,43 @@
 #%%Imports
 import numpy as np
 import matplotlib.pyplot as plt
-from Functions import *
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator
+import CN_Functions as CN
 
 
-#%%Initiali
+#%%Initialisation
 #%%
+Tank_ = CN.Tank(112e-3, 1.19, 6.3588e-6)
+HE = CN.HeatElem(.95, 6e3, np.array([[0.2975], [.7735]]), \
+              np.array([[0.2975], [.7735]]), 2)
+
+DrawTab = np.array([
+                    [2.5, 40, 3], \
+                    [5, 15, 6]\
+                        ])
+#%%
+deltaT = 1 #s
+sim_time = 10 #h
+N = 10
+#%%
+T_init = 25
+T_amb = 25
+T_in = 25
+T_target = 60
+eps = 150
+#%%Simulation
+#%%
+tsol, xVector, sol = CN.CN_meth(Tank_, HE, DrawTab, deltaT, sim_time, N, T_init, T_amb, T_in, T_target, eps)
+
+
+#%%Plotting
+#%%
+
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+tsol_mesh, xVector_mesh = np.meshgrid(tsol/3600, xVector)
+
+surf = ax.plot_surface(tsol_mesh, xVector_mesh, sol[0:N, :], cmap=cm.coolwarm, linewidth=0, antialiased=False)
+fig.colorbar(surf, shrink=10, aspect=5)
+
+plt.show()
